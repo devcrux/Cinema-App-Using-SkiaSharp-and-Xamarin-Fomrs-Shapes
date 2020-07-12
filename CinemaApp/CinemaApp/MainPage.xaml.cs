8e66ffd98f79d1a1f54000516c1703dc208b1e31
Oludayo Alli <sunnymexy@gmail.com>
2020-07-12 20:33:42 +0100

    Add project files.

---
+++ CinemaApp/CinemaApp/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CinemaApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Tickets = GetTickets();
+            this.BindingContext = this;
+        }
+
+        public ObservableCollection<Ticket> Tickets { get; set; }
+        public Ticket SelectedTicket { get; set; }
+
+        private ObservableCollection<Ticket> GetTickets()
+        {
+            return new ObservableCollection<Ticket>
+            {
+                new Ticket { MovieTitle = "Bad Boys For LIfe", Image = "BadBoys.jpg", ShowingDate = DateTime.Now.AddDays(15), Seats = new int[] { 61, 62, 63 } },
+                new Ticket { MovieTitle = "The Old Guard", Image = "OldGuard.jpg", ShowingDate = DateTime.Now.AddDays(22), Seats = new int[] { 111, 112 } },
+                new Ticket { MovieTitle = "Tenet", Image = "Tenet.jpg", ShowingDate = DateTime.Now.AddDays(25), Seats = new int[] { 12, 25, 35 } },
+                new Ticket { MovieTitle = "No Time To Die", Image = "TimeToDie.jpg", ShowingDate = DateTime.Now.AddDays(20), Seats = new int[] { 99 } }
+            };
+        }
+
+        private void TicketSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection != null)
+            {
+                this.Navigation.PushAsync(new SeatsPage(SelectedTicket));
+            }
+        }
+    }
+
+    public class Ticket
+    {
+        public string MovieTitle { get; set; }
+        public DateTime ShowingDate { get; set; }
+        public string Image { get; set; }
+        public int[] Seats { get; set; }
+    }
+}
---
+++ CinemaApp/CinemaApp/SeatsPage.xaml.cs
@@ -0,0 +1,121 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CinemaApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SeatsPage : ContentPage
+    {
+        public SeatsPage(Ticket ticket)
+        {
+            InitializeComponent();
+            SelectedTicket = ticket;
+            Init();
+            this.BindingContext = this;
+        }
+
+        public Ticket SelectedTicket { get; set; }
+        Dictionary<int, int> data = new Dictionary<int, int>();
+        SKPaint availablePaint = new SKPaint() { Style = SKPaintStyle.Stroke, Color = SKColor.Parse("#343352") };
+
+        SKPaint reservedPaint = new SKPaint() { Style = SKPaintStyle.StrokeAndFill, Color = SKColor.Parse("#343352") };
+
+        SKPaint yourSeatPaint = new SKPaint() { Style = SKPaintStyle.StrokeAndFill, Color = SKColor.Parse("#9747FF") };
+
+        SKPaint textPaint = new SKPaint() { TextSize = 40, Color = SKColor.Parse("#343352") };
+
+        private void Init()
+        {
+            var rand = new Random();
+            for (int i = 0; i < 120; i++)
+            {
+                data.Add(i, rand.Next(0, 2));
+            }
+        }
+
+        private void SKCanvasView_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs e)
+        {
+            var canvas = e.Surface.Canvas;
+            var x = 60;
+            var y = 60;
+            var row = -1;
+            var columns = 14;
+            var itemHeight = 40;
+            var itemWidth = 40;
+            var margin = 20;
+            var cornerRadius = 4;
+            var items = 0;
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                if(items == 0)
+                {
+                    row += 1;
+                    items = GetColumn(row);
+                    var count = (columns - items) / 2;
+                    var offset = (count * itemWidth) + (count * margin);
+                    x = 60 + offset;
+                    y = (itemWidth + ((itemWidth + margin) * row));
+                }
+                else
+                {
+                    x += itemHeight + margin;
+                }
+
+                var seatColorIndex = data[i];
+
+                if (SelectedTicket.Seats.Any(z => z == i))
+                    seatColorIndex = 2;
+
+                canvas.DrawRoundRect(new SKRoundRect(new SKRect(x, y, x + itemHeight, y + itemWidth), cornerRadius), GetColor(seatColorIndex));
+
+                items -= 1;
+
+                if (items == 0)
+                {
+                    canvas.DrawText($"{row + 1}", 0, y + margin + (itemHeight / 2), textPaint);
+                }
+            }
+        }
+
+
+        SKPaint GetColor(int seatColor)
+        {
+            switch (seatColor)
+            {
+                case 0:
+                default:
+                    return availablePaint;
+                case 1:
+                    return reservedPaint;
+                case 2:
+                    return yourSeatPaint;
+            }
+        }
+
+        private int GetColumn(int row)
+        {
+            switch (row)
+            {
+                case 0:
+                    return 8;
+                case 1:
+                case 9:
+                    return 10;
+                case 2:
+                case 3:
+                case 8:
+                    return 12;
+                default:
+                    return 14;
+            }
+        }
+    }
+}
